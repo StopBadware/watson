b337bf0c68d67643d326c37a06c9bf19494b934d
Matthew Frost <mfrost@stopbadware.org>
2013-05-08 12:07:50 -0400

    WTSN-11 json parsing

--- app/controllers/Rest.scala
+++ app/controllers/Rest.scala
@@ -1,29 +1,30 @@
 package controllers
 
+import scala.collection.immutable.{HashMap, HashSet}
 import play.api._
+import play.api.libs.json._
 import play.api.mvc._
 
 object Rest extends Controller {
 
-	def index = Action {
-		Ok(views.html.index("this will be a rest endpoint"))
-  }
-	
-	def timeoflast(source: String) = Action {
-		Ok(views.html.index("timeoflast for "+source))
+	def timeoflast(source: String) = Action { request =>
+		Ok(views.html.index("timeoflast for "+source))	//TODO: WTSN-20
   }
 	
 	def blacklist(source: String) = Action { request =>
-	  println(request.body)	//DELME
-	  
-		Ok(views.html.index("blacklist"))
+	  println(request.body.asJson)	//DELME
+	  val json = (request.body.asJson).get
+//	  val foo = (json \ "object").asOpt[Map[String, String]]
+//	  println(foo.getOrElse("not provided"))
+//	  println(foo.get("a"))
+		Ok("blacklist")
   }
 	
-	def cleanlist(source: String) = Action {
-		Ok(views.html.index("cleanlist"))
+	def cleanlist(source: String) = Action { request =>
+		Ok("cleanlist")
   }
 	
-	def appeals(source: String) = Action {
-		Ok(views.html.index("appeals"))
+	def appeals(source: String) = Action { request =>
+		Ok("appeals")
   }
 }
