5b8fa682166466e9a85e1496c710654a67c9d5fb
Matthew Frost <mfrost@stopbadware.org>
2013-05-08 10:31:26 -0400

    WTSN-11 addes source param for rest endpoints

--- app/controllers/Rest.scala
+++ app/controllers/Rest.scala
@@ -8,4 +8,20 @@ object Rest extends Controller {
 	def index = Action {
 		Ok(views.html.index("this will be a rest endpoint"))
   }
+	
+	def timeoflast(source: String) = Action {
+		Ok(views.html.index("timeoflast for "+source))
+  }
+	
+	def blacklist(source: String) = Action {
+		Ok(views.html.index("blacklist"))
+  }
+	
+	def cleanlist(source: String) = Action {
+		Ok(views.html.index("cleanlist"))
+  }
+	
+	def appeals(source: String) = Action {
+		Ok(views.html.index("appeals"))
+  }
 }
