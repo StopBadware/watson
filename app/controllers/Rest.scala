a54f321eceee0cadce74e3c7476bbb0aa7ae9abe
Matthew Frost <mfrost@stopbadware.org>
2013-05-10 16:43:47 -0400

    WTSN-11 blist entries ready to write to db

--- app/controllers/Rest.scala
+++ app/controllers/Rest.scala
@@ -1,7 +1,7 @@
 package controllers
 
 import scala.collection.immutable.{HashMap, HashSet}
-import scala.collection.JavaConversions._ 	//DELME?
+import scala.collection.JavaConversions._
 import scala.io.Source
 import play.api._
 import play.api.libs.json._
@@ -15,24 +15,18 @@ object Rest extends Controller {
   }
 	
 	def blacklist(source: String) = Action(parse.temporaryFile) { request =>
-	  Logger.debug("SUBROUTINE BEGIN")	//DELME
 	  val str = Source.fromFile(request.body.file).mkString	
-	  Logger.debug(str.length.toString)	//DELME
-	  Logger.debug("PARSING BEGIN")			//DELME
 	  val mapper = new ObjectMapper
 	  val json = mapper.readTree(str)	
-	  Logger.debug("PARSING COMPLETE")	//DELME
-//	  val foo = json.get("1367412595")
-//	  println(foo.getClass+"\t"+foo.size)
-//	  val foo = json.get("1367412595")
-	  val foo = json.fieldNames.toList
-	  foo.foreach(f => println(f))
-//	  val foo = json.path(1)
-//	  println(foo.size())
-//	  println(foo.get(0))
-//	  println(foo.path(0).toString())
-	  Logger.debug("SUBROUTINE END")		//DELME
-		Ok("blacklist")
+	  val blTimes = json.fieldNames.toList
+	  blTimes.foreach { bltime =>
+	    val blacklist = json.get(bltime).iterator.toList
+	    blacklist.foreach { entry =>
+	      //TODO: store entries in db
+	      println(entry)		//DELME
+	    }
+	  }
+		Ok
   }
 	
 	def cleanlist(source: String) = Action { request =>
