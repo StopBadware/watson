2177200e72081537165c688badd7142d2036a1a3
Matthew Frost <mfrost@stopbadware.org>
2013-05-10 16:53:04 -0400

    WTSN-11 moved file=>json to own subroutine

--- app/controllers/Rest.scala
+++ app/controllers/Rest.scala
@@ -1,30 +1,35 @@
 package controllers
 
+import java.io.File
 import scala.collection.immutable.{HashMap, HashSet}
 import scala.collection.JavaConversions._
 import scala.io.Source
 import play.api._
 import play.api.libs.json._
 import play.api.mvc._
-import com.fasterxml.jackson.databind.ObjectMapper
+import com.fasterxml.jackson.databind.{JsonNode, ObjectMapper}
 
 object Rest extends Controller {
+  
+  def getJson(file: File): JsonNode = {
+    val str = Source.fromFile(file).mkString
+    val mapper = new ObjectMapper
+    return mapper.readTree(str)
+  }
 
 	def timeoflast(source: String) = Action { request =>
 		Ok(views.html.index("timeoflast for "+source))	//TODO: WTSN-20
   }
 	
 	def blacklist(source: String) = Action(parse.temporaryFile) { request =>
-	  val str = Source.fromFile(request.body.file).mkString	
-	  val mapper = new ObjectMapper
-	  val json = mapper.readTree(str)	
+	  val json = getJson(request.body.file)
 	  val blTimes = json.fieldNames.toList
 	  blTimes.foreach { bltime =>
 	    val blacklist = json.get(bltime).iterator.toList
 	    blacklist.foreach { entry =>
 	      //TODO: store entries in db
-	      println(entry)		//DELME
 	    }
+	    Logger.debug("this set: "+blacklist.size)		//DELME
 	  }
 		Ok
   }
