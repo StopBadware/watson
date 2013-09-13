6d64233772b37d3fc646f5f5367c72bf3d060865
Matthew Frost <mfrost@stopbadware.org>
2013-09-13 15:11:04 -0400

    WTSN-11 connect to mongodb

--- app/controllers/DbHandler.scala
+++ app/controllers/DbHandler.scala
@@ -6,9 +6,15 @@ import com.mongodb.casbah.Imports._
 
 object DbHandler extends Controller {
   
+  val mongoUrl = MongoClientURI(sys.env("MONGO_URL"))
+  val db = MongoClient(mongoUrl).getDB(mongoUrl.database.get)
+  
   def importBlacklist(blist: Blacklist) {
     println("size: "+blist.uris.size,"source: "+blist.source,"time: "+blist.blTime,"isDiff: "+blist.isDifferential) //DELME WTSN-11
     blist.uris.foreach(upsertUri(_, blist.source, blist.blTime))
+    //DELME
+    println(db.collectionNames)
+    //DELME
     if (blist.isDifferential) {
     	//TODO WTSN-11 update entries for source not in blist with bltimes < bl.time
     }
