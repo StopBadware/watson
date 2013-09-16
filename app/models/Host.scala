a89e1228c922f713ba47c9644d0624e18e78aa09
Matthew Frost <mfrost@stopbadware.org>
2013-09-16 16:27:05 -0400

    WTSN-11 reverse host labels

--- app/models/Host.scala
+++ app/models/Host.scala
@@ -6,8 +6,6 @@ case class Host(host: String) {
 
 object Host {
   
-  def reverse(host: String): String = {
-    //TODO WTSN-11 reverse host
-    "TODO"
-  }
+  def reverse(host: String): String = host.split("\\.").reverse.mkString(".")
+
 }
