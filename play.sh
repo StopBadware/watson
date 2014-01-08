bc5c7cb8d429fcd789320a9d49fc264de9e57853
Matthew Frost <mfrost@stopbadware.org>
2014-01-08 10:33:49 -0500

    WTSN-42 remove -J from JAVA_OPTS when starting with script

--- play.sh
+++ play.sh
@@ -5,6 +5,10 @@ while read line
 do
     commented='^#.*'
     if [[ ! $line =~ $commented ]]; then
+        jvmOpt='.*\s-J-X.+'
+        if [[ $line =~ $jvmOpt ]]; then
+            line=${line//-J-X/-X}
+        fi
     	cmd="$cmd$line "
     fi
 done < .env
