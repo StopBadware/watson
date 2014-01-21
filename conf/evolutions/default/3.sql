c480537f337f1a8d9fd43521bcac233fb0fce00e
Matthew Frost <mfrost@stopbadware.org>
2014-01-21 11:48:13 -0500

    WTSN-30 insert email template placeholders

--- conf/evolutions/default/3.sql
+++ conf/evolutions/default/3.sql
@@ -3,12 +3,18 @@
 # --- !Ups
 
 CREATE TABLE email_templates (
-  name VARCHAR(20) PRIMARY KEY,
+  name VARCHAR(24) PRIMARY KEY,
   subject VARCHAR(128) NOT NULL,
   body VARCHAR(4096) NOT NULL,
   modified_by INTEGER DEFAULT NULL REFERENCES users (id) ON DELETE SET NULL,
   modified_at TIMESTAMP NOT NULL DEFAULT NOW()
 );
+
+INSERT INTO email_templates (name, subject, body) VALUES  
+  ('NoLongerBlacklisted', '[StopBadware] subject', 'Use [URI] or [SAFE_URI] to insert links to URI or Clearinghouse entry'), 
+  ('ReviewClosedBad', '[StopBadware] subject', 'Use [URI] or [SAFE_URI] to insert links to URI or Clearinghouse entry'),
+  ('ReviewClosedCleanTts', '[StopBadware] subject', 'Use [URI] or [SAFE_URI] to insert links to URI or Clearinghouse entry'), 
+  ('ReviewRequestReceived', '[StopBadware] subject', 'Use [URI] or [SAFE_URI] to insert links to URI or Clearinghouse entry');
 
 # --- !Downs
 
