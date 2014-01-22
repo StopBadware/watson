21ba001615c9fe5277ab31d04d7d809a8a117ec9
Matthew Frost <mfrost@stopbadware.org>
2014-01-22 16:00:53 -0500

    WTSN-31 creating Review model

--- app/models/Review.scala
+++ app/models/Review.scala
@@ -5,7 +5,69 @@ import play.api.db._
 import play.api.Play.current
 import play.api.Logger
 import org.postgresql.util.PSQLException
+import scala.util.Try
+import models.enums.ReviewStatus
 
-class Review {
+case class Review(
+	  id: Int, 
+	  uriId: Int,
+	  reviewedBy: Option[Int],
+	  verifiedBy: Option[Int],
+	  reviewDataId: Option[Int],
+	  reviewTags: List[Int],
+	  status: ReviewStatus,
+	  createdAt: Long,
+	  statusUpdatedAt: Long
+  ) {
 
+  
+  def reviewed(verdict: ReviewStatus, reviewer: Int, reviewData: Option[Int]=None): Boolean = DB.withConnection { implicit conn =>
+    return false 		//TODO WTSN-31
+  }
+  
+  def reject(verifier: Int, comments: String): Boolean = DB.withConnection { implicit conn =>
+    return false 		//TODO WTSN-31
+  }
+  
+  def close(closeAs: ReviewStatus, verifier: Option[Int]=None): Boolean = DB.withConnection { implicit conn =>
+    return false 		//TODO WTSN-31
+  }
+  
+  
+//  def updateStatus(description: Option[String], hexColor: String): Boolean = DB.withConnection { implicit conn =>
+//    return try {
+//      SQL("UPDATE reviews SET description={description}, hex_color={hexColor} WHERE id={id}")
+//        .on("id" -> id, "description" -> Try(description.get).getOrElse(null), "hexColor" -> hexColor)
+//        .executeUpdate() > 0
+//    } catch {
+//      case e: PSQLException => Logger.error(e.getMessage)
+//      false
+//    }
+//  }	//DELME WTSN-31
+  
+  def delete(): Boolean = DB.withConnection { implicit conn =>
+    return try {
+      SQL("DELETE FROM reviews WHERE id={id}").on("id" -> id).executeUpdate() > 0
+    } catch {
+      case e: PSQLException => Logger.error(e.getMessage)
+      false
+    }
+  }
+  
+}
+
+object Review {
+  
+  def create(uriId: Int): Boolean = DB.withConnection { implicit conn =>
+    return false	//TODO WTSN-31
+  }
+  
+  def find(id: Int): Option[Review] = DB.withConnection { implicit conn =>
+    return None		//TODO WTSN-31
+  }
+  
+  def findByUris(uriId: Int): List[Review] = DB.withConnection { implicit conn =>
+    return List()		//TODO WTSN-31
+  }
+  
 }
---
+++ test/models/ReviewSpec.scala
@@ -0,0 +1,76 @@
+package models
+
+import org.specs2.mutable._
+import org.junit.runner._
+import org.specs2.runner._
+import play.api.test._
+import play.api.test.Helpers._
+
+@RunWith(classOf[JUnitRunner])
+class ReviewSpec extends Specification {
+
+  "Review" should {
+    
+    "create a review" in {
+      running(FakeApplication()) {
+        true must beFalse		//DELME WTSN-31
+      }
+    }
+    
+    "delete a review" in {
+      running(FakeApplication()) {
+        true must beFalse		//DELME WTSN-31
+      }
+    }
+    
+    "update a review's status" in {
+      running(FakeApplication()) {
+        true must beFalse		//DELME WTSN-31
+      }
+    }
+    
+    "close/verify a review" in {
+      running(FakeApplication()) {
+        true must beFalse		//DELME WTSN-31
+      }
+    }
+    
+    "reject a review" in {
+      running(FakeApplication()) {
+        true must beFalse		//DELME WTSN-31
+      }
+    }
+    
+    "add a tag" in {
+      running(FakeApplication()) {
+        true must beFalse		//DELME WTSN-31
+      }
+    }
+    
+    "remove a tag" in {
+      running(FakeApplication()) {
+        true must beFalse		//DELME WTSN-31
+      }
+    }
+    
+    "find a review" in {
+      running(FakeApplication()) {
+        true must beFalse		//DELME WTSN-31
+      }
+    }
+    
+    "find reviews by tag name" in {
+      running(FakeApplication()) {
+        true must beFalse		//DELME WTSN-31
+      }
+    }
+    
+    "find reviews by Uri" in {
+      running(FakeApplication()) {
+        true must beFalse		//DELME WTSN-31
+      }
+    }
+    
+  }
+  
+}
