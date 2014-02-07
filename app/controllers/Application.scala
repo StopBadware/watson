0e6dadeaeefea4a1cd0dc15ca94756dafcb3c0a0
Matthew Frost <mfrost@stopbadware.org>
2014-02-07 16:58:30 -0500

    WTSN-52 parse registration json

--- app/controllers/Application.scala
+++ app/controllers/Application.scala
@@ -5,7 +5,7 @@ import play.api.mvc._
 import play.api.libs.json._
 import routes.javascript._
 
-object Application extends Controller {
+object Application extends Controller with JsonMapper {
   
   def index = Action {
     Ok(views.html.index("Watson"))
@@ -19,12 +19,12 @@ object Application extends Controller {
     Ok(views.html.register())
   }
   
-  def createAccount = Action { implicit request =>
-    println(request)			//DELME WTSN-52
-    println(request.body)	//DELME WTSN-52
-    val created = true		//TODO WTSN-52
-    val msg = ""					//TODO WTSN-52
-     Ok(Json.obj("created" -> created, "msg" -> msg))
+  def createAccount = Action(parse.json) { implicit request =>
+    val email = request.body.\("email").asOpt[String].getOrElse("")
+    val pw = request.body.\("pw").asOpt[String].getOrElse("")
+    val created = true		//TODO WTSN-52 create account
+    val msg = ""					//TODO WTSN-52 create should return Try instead of Boolean
+    Ok(Json.obj("created" -> created, "msg" -> msg))
   }
   
   def untrail(path: String) = Action {
