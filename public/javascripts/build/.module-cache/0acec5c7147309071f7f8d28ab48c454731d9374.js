b33e98a16a3f0bed7b133749d7c7d69cd6e06d3d
Matthew Frost <mfrost@stopbadware.org>
2014-03-07 17:06:57 -0500

    WTSN-18 ignore react cache

--- public/javascripts/build/.module-cache/0acec5c7147309071f7f8d28ab48c454731d9374.js
+++
@@ -1,5 +0,0 @@
-/** @jsx React.DOM */
-React.renderComponent(
-  React.DOM.h3(null, "Associated URIs"),
-  document.getElementById('example')
-);
--- public/javascripts/build/.module-cache/fdb720aede7aec45d2e14c791155430aec8c382a.js
+++
@@ -1,5 +0,0 @@
-/** @jsx React.DOM */
-React.renderComponent(
-  React.DOM.h1(null, "Hello from react"),
-  document.getElementById('example')
-);
