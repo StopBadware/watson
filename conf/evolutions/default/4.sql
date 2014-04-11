d24b916c8e5cec323c3ba6203b1df29b32fe0a3a
Matthew Frost <mfrost@stopbadware.org>
2014-04-11 10:04:15 -0400

    WTSN-16 added CR notes and sources

--- conf/evolutions/default/4.sql
+++ conf/evolutions/default/4.sql
@@ -1,14 +1,23 @@
-# community_reports related schemas
+# community report related schemas
 
 # --- !Ups
 
-CREATE TABLE community_report_types (
+CREATE TABLE cr_types (
   id SERIAL PRIMARY KEY,
   cr_type VARCHAR(64) NOT NULL,
   created_at TIMESTAMP NOT NULL DEFAULT NOW()
 );
 
-CREATE INDEX ON community_report_types (cr_type);
+CREATE INDEX ON cr_types (cr_type);
+
+CREATE TABLE cr_sources (
+  id SERIAL PRIMARY KEY,
+  short_name VARCHAR(6) NOT NULL,
+  full_name VARCHAR(32) NOT NULL, 
+  created_at TIMESTAMP NOT NULL DEFAULT NOW()
+);
+
+CREATE INDEX ON cr_sources (short_name);
 
 CREATE TABLE community_reports (
   id SERIAL PRIMARY KEY,
@@ -16,15 +25,27 @@ CREATE TABLE community_reports (
   ip BIGINT DEFAULT NULL,
   description VARCHAR(2048) DEFAULT NULL,
   bad_code VARCHAR(2048) DEFAULT NULL,
-  cr_type_id INTEGER DEFAULT NULL REFERENCES community_report_types (id) ON DELETE SET NULL,
-  sbw_note VARCHAR(512) DEFAULT NULL,
+  cr_type_id INTEGER DEFAULT NULL REFERENCES cr_types (id) ON DELETE SET NULL,
+  reported_via INTEGER DEFAULT NULL REFERENCES cr_sources (id) ON DELETE SET NULL,
   reported_at TIMESTAMP NOT NULL DEFAULT NOW()
 );
 
 CREATE INDEX ON community_reports (uri_id);
-CREATE INDEX ON community_reports (cr_type_id, reported_at);
+CREATE INDEX ON community_reports (cr_type_id, reported_via, reported_at);
+
+CREATE TABLE cr_notes (
+  id SERIAL PRIMARY KEY,
+  cr_id INTEGER NOT NULL REFERENCES community_reports (id) ON DELETE CASCADE,
+  author INTEGER NOT NULL REFERENCES users (id) ON DELETE RESTRICT,
+  note VARCHAR(512) NOT NULL,
+  created_at TIMESTAMP NOT NULL DEFAULT NOW()
+);
+
+CREATE INDEX ON cr_notes (cr_id, author);
 
 # --- !Downs
 
+DROP TABLE IF EXISTS cr_notes CASCADE;
 DROP TABLE IF EXISTS community_reports CASCADE;
-DROP TABLE IF EXISTS community_report_types CASCADE;
+DROP TABLE IF EXISTS cr_sources CASCADE;
+DROP TABLE IF EXISTS cr_types CASCADE;
