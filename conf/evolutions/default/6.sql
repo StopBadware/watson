fe0a8a0bd47b8e8fda871e3fd640c1525c3edd19
Matthew Frost <mfrost@stopbadware.org>
2014-05-06 16:44:24 -0400

    WTSN-14 added host/ip/asn schemas

---
+++ conf/evolutions/default/6.sql
@@ -0,0 +1,38 @@
+# hosts/ips/asns related schemas
+
+# --- !Ups
+
+CREATE TABLE autonomous_systems (
+  number INTEGER PRIMARY KEY,
+  name VARCHAR(256) NOT NULL,
+  country CHAR(2) NOT NULL,
+  updated_at TIMESTAMP NOT NULL DEFAULT NOW()
+);
+
+CREATE INDEX ON autonomous_systems (name, country);
+
+CREATE TABLE ip_asn_mappings (
+  id SERIAL PRIMARY KEY,
+  ip BIGINT NOT NULL,
+  asn INTEGER NOT NULL REFERENCES autonomous_systems (number) ON DELETE RESTRICT,
+  mapped_at TIMESTAMP NOT NULL DEFAULT NOW()
+);
+
+CREATE INDEX ON ip_asn_mappings (ip);
+CREATE INDEX ON ip_asn_mappings (asn);
+
+CREATE TABLE host_ip_mappings (
+  id SERIAL PRIMARY KEY,
+  reversed_host VARCHAR(256) NOT NULL,
+  ip BIGINT NOT NULL,
+  resolved_at TIMESTAMP NOT NULL DEFAULT NOW()
+);
+
+CREATE INDEX ON host_ip_mappings (reversed_host);
+CREATE INDEX ON host_ip_mappings (ip);
+
+# --- !Downs
+
+DROP TABLE IF EXISTS host_ip_mappings CASCADE;
+DROP TABLE IF EXISTS ip_asn_mappings CASCADE;
+DROP TABLE IF EXISTS autonomous_systems CASCADE;
